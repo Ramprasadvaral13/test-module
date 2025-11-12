132c4b1426b994977357b069125edc182f6ad878
Ramprasad <ramprasad@Ramprasads-MacBook-Air.local>
2025-11-12 17:24:38 +0530

    test-commit-d

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "test-vpc" {
+resource "aws_vpc" "test_vpc" {
     cidr_block = var.vpc_cidr
     enable_dns_hostnames = true
     enable_dns_support = true
@@ -6,12 +6,12 @@ resource "aws_vpc" "test-vpc" {
 }
 
 resource "aws_internet_gateway" "test_igw" {
-    vpc_id = aws_vpc.test-vpc.id
+    vpc_id = aws_vpc.test_vpc.id
   
 }
 
 resource "aws_subnet" "test_subnet" {
-    vpc_id = aws_vpc.test-vpc.id
+    vpc_id = aws_vpc.test_vpc.id
     for_each = var.subnets
     cidr_block = each.value.cidr
     availability_zone = each.value.az
@@ -26,7 +26,7 @@ resource "aws_eip" "test_eip" {
 
 resource "aws_nat_gateway" "test_nat" {
     allocation_id = aws_eip.test_eip.id
-    subnet_id = aws_subnet.test-subnet[
+    subnet_id = aws_subnet.test_subnet[
         keys({
             for k,s in var.subnets : k=>s if s.public == true
         })[0]
@@ -35,7 +35,7 @@ resource "aws_nat_gateway" "test_nat" {
 }
 
 resource "aws_route_table" "test_public_rtb" {
-    vpc_id = aws_vpc.test-vpc.id
+    vpc_id = aws_vpc.test_vpc.id
     route {
         cidr_block = var.route_cidr
         gateway_id = aws_internet_gateway.test_igw.id
@@ -49,12 +49,12 @@ resource "aws_route_table_association" "test_public_rtba" {
    }
 
    route_table_id = aws_route_table.test-public-rtb.id
-   subnet_id = aws_subnet.test-subnet[each.key].id
+   subnet_id = aws_subnet.test_subnet[each.key].id
   
 }
 
 resource "aws_route_table" "test_private_rtb" {
-    vpc_id = aws_vpc.test-vpc.id
+    vpc_id = aws_vpc.test_vpc.id
     route {
         cidr_block = var.route_cidr
         gateway_id = aws_nat_gateway.test_nat.id
@@ -67,7 +67,7 @@ resource "aws_route_table_association" "test_private_rtba" {
       for k,s in var.subnets : k=>s if s.public == false 
     }
     route_table_id = aws_route_table.test_private_rtb.id
-    subnet_id = aws_subnet.test-subnet[each.key].id
+    subnet_id = aws_subnet.test_subnet[each.key].id
   
 
 }
